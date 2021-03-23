95626f3a0337fa46bae39db394be84d4634859d7
Your NameSaltedFish <2514730461@qq.com>
2021-03-23 22:00:56 +0800

    更改ignore文件内容

--- prj/Objects/context_rvds.d
+++
@@ -1,1 +0,0 @@
-.\objects\context_rvds.o: ..\src\rt_thread\libcpu\arm\cortex-m3\context_rvds.S
--- prj/Objects/core_cm3.d
+++
@@ -1,2 +0,0 @@
-.\objects\core_cm3.o: ..\src\stm32f103_libraries\CMSIS\CM3\CoreSupport\core_cm3.c
-.\objects\core_cm3.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
